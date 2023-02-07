68aca1d33a0c8d41ef233b8f370d634f7381ac58
L4qqer <firatege52@gmail.com>
2023-02-07 21:44:37 +0300

    Update/Restart of Ball

--- Assets/_Script/Ball.cs
+++ Assets/_Script/Ball.cs
@@ -4,22 +4,23 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
-  
+
     public float speed = 25;
+
     void Start()
     {
         randomTrans();
     }
 
-    
+
     void Update()
     {
-        
+
     }
 
-  
-    
-    
+
+
+
     public void randomTrans() //Random Start Func
     {
 
@@ -27,15 +28,24 @@ public class Ball : MonoBehaviour
         switch (randTrans)
         {
             case 1:
-                GetComponent<Rigidbody2D>().velocity = Vector2.right*speed;
+                GetComponent<Rigidbody2D>().velocity = Vector2.right * speed;
                 break;
             case 2:
-                GetComponent<Rigidbody2D>().velocity = Vector2.left*speed;
+                GetComponent<Rigidbody2D>().velocity = Vector2.left * speed;
                 break;
         }
 
 
     }
+
+    void OnTriggerEnter2D(Collider2D other) //Pushing the ball again
+    {
+        if (other.gameObject.tag == "Kale")
+        {
+            GetComponent<Transform>().position = new Vector2(0, 0);
+            randomTrans();
+        }
+    }
+
+
 }
-
-
